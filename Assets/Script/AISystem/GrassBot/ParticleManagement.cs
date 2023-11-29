9b64567e4024799d6cfe3567161b5a0f121ff00d
UniverseJason <011203jasonlee@gmail.com>
2023-11-29 02:19:17 -0600

    Update: Particle System

---
+++ Assets/Script/AISystem/GrassBot/ParticleManagement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Script.AISystem.GrassBot
+{
+    public class ParticleManagement : MonoBehaviour
+    {
+        public ParticleSystem IdleParticle;
+        public ParticleSystem MoveParticle;
+        public ParticleSystem Attack1Particle;
+        public ParticleSystem Attack2Particle;
+
+        public void ParticleSwitch(ParticleSystem p)
+        {
+            StopAllParticle();
+            p.Play();
+        }
+
+        public void StopAllParticle()
+        {
+            IdleParticle.Stop();
+            MoveParticle.Stop();
+            Attack1Particle.Stop();
+            Attack2Particle.Stop();
+        }
+    }
+}
